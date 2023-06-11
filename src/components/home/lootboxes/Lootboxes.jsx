e5a14c7966681dde44088ad48c19f26769c3cc71
padi-dev-quocnd <quocnd@paditech.com>
2023-06-11 22:28:23 +0700

    update

--- src/components/home/lootboxes/Lootboxes.jsx
+++ src/components/home/lootboxes/Lootboxes.jsx
@@ -56,7 +56,7 @@ export default function Lootboxes() {
       className="p-4 sm:px-[48px] md:px-[72px] xl:px-[104px] py-[128px] bg-[rgb(214,244,251)]"
       id="Lootboxes"
     >
-      <div className="min-h-[311px] border-[0.1875rem] border-[#5b5b5b] rounded-[4rem] 2xl:rounded-[6.25rem] overflow-hidden bg-[#fffef5] shadow-card">
+      <div className="min-h-[311px] 2xl:h-[569px] border-[0.1875rem] border-[#5b5b5b] rounded-[4rem] 2xl:rounded-[6.25rem] overflow-hidden bg-[#fffef5] shadow-card">
         <div className="bg-[#a6e8fe] border-b-[0.1875rem] border-[#000] flex items-center gap-3 pl-[4.5rem] py-4">
           <div className="w-[1.5rem] h-[1.5rem] rounded-full bg-[#eb6e6e] border-[0.125rem] border-[#000]"></div>
           <div className="w-[1.5rem] h-[1.5rem] rounded-full bg-[#ffdea9] border-[0.125rem] border-[#000]"></div>
@@ -64,24 +64,26 @@ export default function Lootboxes() {
         </div>
         <div className="py-[2rem] 2xl:py-[96px] px-[2rem] 2xl:px-[150px]">
           <div className="max-w-[80rem] w-full flex flex-col-reverse min-[1028px]:flex-row items-center ">
-            <div className="xl:mr-[2rem]">
-              <h1 className="my-4 text-[2.5rem] leading-[1.025em] sm:leading-none min-[1440px]:text-[2.875rem] 2xl:text-[3.125rem] max-w-[17rem] md:max-w-[36rem] min-[1028px]:max-w-[30rem] min-[1440px]:max-w-[35rem] font-Extra text-center ">
-                Open lootboxes to win $BIG!
+            <div className="xl:mr-[2rem] 2xl:px-6">
+              <h1 className="my-4 text-[2.5rem] leading-[1.025em] sm:leading-none min-[1440px]:text-[2.875rem] 2xl:text-[3.125rem] max-w-[17rem] md:max-w-[36rem] min-[1028px]:max-w-[30rem] min-[1440px]:max-w-[35rem] font-Extra text-center font-900">
+                Open lootboxes to win{" "}
+                <span className="font-Extra font-[900]">$</span>
+                BIG!
               </h1>
-              <h2 className="my-[20px] font-bold text-[#f48d97]  max-w-[17rem] md:max-w-[36rem] min-[1028px]:max-w-[31rem] min-[1440px]:max-w-[33rem] mx-auto text-center text-[24px]">
+              <h2 className="mb-[20px] mt-[35px] font-bold text-[#f48d97] leading-7  max-w-[17rem] md:max-w-[36rem] min-[1028px]:max-w-[31rem] min-[1440px]:max-w-[33rem] mx-auto text-center text-[24px]">
                 Enter our presale in a fun way! Open lootboxes to get a random
                 amount of $BIG tokens!
               </h2>
-              <div className="mt-4 flex flex-col sm:flex-row justify-center items-center gap-3 max-w-[17rem] md:max-w-[36rem] min-[1028px]:max-w-[28rem] min-[1440px]:max-w-[35rem]">
+              <div className="mt-4 flex flex-col sm:flex-row justify-center items-center  max-w-[17rem] md:max-w-[36rem] min-[1028px]:max-w-[28rem] min-[1440px]:max-w-[35rem]">
                 <button
-                  className="bg-[#f38590] max-w-[16rem] w-full whitespace-nowrap text-white border-[5px] text-[1.125rem] 2xl:text-[1.25rem] hover:font-semibold border-[#F9C7CC] rounded-full  py-[8px] px-[24px] font-semibold hover:bg-[#F9C7CC] hover:text-black transition-all duration-300"
+                  className="m-[.5rem] bg-[#f38590] max-w-[13rem] w-full whitespace-nowrap text-white border-[5px] text-[1.125rem] 2xl:text-[1.25rem] hover:font-semibold border-[#F9C7CC] rounded-full  py-[8px] px-[24px] font-semibold hover:bg-[#F9C7CC] hover:text-black transition-all duration-300"
                   onClick={handleClaim}
                 >
                   Buy Lootboxes
                 </button>
 
                 <button
-                  className="bg-[#f38590] max-w-[16rem] w-full whitespace-nowrap text-white border-[5px] text-[1.125rem] 2xl:text-[1.25rem] hover:font-semibold border-[#F9C7CC] rounded-full  py-[8px] px-[24px] font-semibold hover:bg-[#F9C7CC] hover:text-black transition-all duration-300"
+                  className="m-[.5rem] bg-[#f38590] max-w-[13rem] w-full whitespace-nowrap text-white border-[5px] text-[1.125rem] 2xl:text-[1.25rem] hover:font-semibold border-[#F9C7CC] rounded-full  py-[8px] px-[24px] font-semibold hover:bg-[#F9C7CC] hover:text-black transition-all duration-300"
                   onClick={() => {
                     window.open(
                       "https://opensea.io/collection/big-eyes-lootbox-cards",
@@ -94,7 +96,7 @@ export default function Lootboxes() {
               </div>
             </div>
             <div className="max-w-[30rem] xl:ml-[2rem]  relative">
-              <div className=" -left-[30%] min-[1028px]:left-0 2xl:left-2/4 relative">
+              <div className=" -left-[30%] min-[1028px]:left-0 2xl:left-[20%] relative">
                 <Image
                   src="https://buy1.bigeyes.space/img/treasure-chest/treasure-chest.webp"
                   className="hidden sm:block"
